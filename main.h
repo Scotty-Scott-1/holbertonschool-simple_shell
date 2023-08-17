7697137e61756cc0564337d0a74fa071d0dc5cae
letihab <letihabib1822@gmail.com>
2023-08-17 11:41:54 +0200

    simple shell

--- main.h
+++ main.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <fcntl.h>
+#include <string.h>
 
 int main(int argc, char **argv);
 void shell_strtok(char *input,  char **argv);
+void exec_command(char*command);
 
 #endif /* MAIN_H */
