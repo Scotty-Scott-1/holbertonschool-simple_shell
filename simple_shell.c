2a31d2ec8a4a56e7127aca0bcd64888a6d26c0d5
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-20 16:26:55 +0200

    Updated to main from testing directory

--- simple_shell.c
+++ simple_shell.c
@@ -1,18 +1,14 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <fcntl.h>
+#include <unistd.h>
 #include <string.h>
 #include "main.h"
 
 
 int main(int ac, char **argv)
 {
-
-	char *input = NULL; /*inputcopy = NULL;*/
+	ssize_t read_result = 0;
+	char *input = NULL;
 	size_t len = 0;
 	char *input_copy = NULL;
 	int i = 0;
@@ -24,24 +20,35 @@ int main(int ac, char **argv)
 	/*	printf("$ ");*/
 		read_result = getline(&input, &len, stdin);
 
-	argc += 1;
-	do {
-		/*printf("$ ");*/
-		read = getline(&input, &len, stdin);
-		if (read == -1)
+		if(read_result == -1)
 		{
-			if (feof(stdin))
-			{
 			break;
-			}
 		}
-		shell_strtok(input, argv);
 
-	} while (1);
-	free(input);
+		input_copy = copy_input(read_result, input);
+
+		argv = tokenize_array(input, argv, input_copy);
+
+		execute_command(argv, counter_nb, input, progam_name);
+		counter_nb++;
+
+		for (i = 0;argv[i] != NULL; i++)
+		{
+			free(argv[i]);
+		}
+
+		free(argv);
+		free(input_copy);
+	}
+free(input);
 
 return(0);
 
-	return (0);
-}
 
+
+
+
+
+
+
+}
